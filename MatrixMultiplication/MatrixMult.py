b37f70784571e53103ab736f46de669590abe92d
Burnsy2100 <burnaidanj@gmail.com>
2025-07-11 00:08:26 +0100

    Update MatrixMult.py

--- MatrixMultiplication/MatrixMult.py
+++ MatrixMultiplication/MatrixMult.py
@@ -1,26 +1,20 @@
 # Imports
 
-import numpy as np
-
 # Functions
 def inputSizes():
-    global nRows1
-    global nColumns1
-    global nRows2
-    global nColumns2
     nRows1 = int(input("Enter number of rows for the first matrix/vector: "))
     nColumns1 = int(input("Enter number of columns for the first matrix/vector: "))
     nRows2 = int(input("Enter number of rows for the second matrix/vector: "))
     nColumns2 = int(input("Enter number of columns for the second matrix/vector: "))
-    checkSizesValid()
+    checkSizesValid(nRows1,nColumns1,nRows2,nColumns2)
 
-def checkSizesValid():
+def checkSizesValid(nRows1, nColumns1, nRows2, nColumns2):
     if nColumns1 == nRows2:
-        enterValues()
+        enterValues(nRows1, nColumns1, nRows2, nColumns2)
     else:
         print("Cannot be multiplied!")
 
-def enterValues():
+def enterValues(nRows1, nColumns1, nRows2, nColumns2):
     global m1
     global m2
     print("Matrix/Vector 1")
@@ -39,13 +33,18 @@ def enterValues():
             m2[rows][columns] = tempNum
             for i in range(0, len(m2)):
                 print(m2[i])
-    solveAndOutput()
+    solveAndOutput(nRows1, nColumns1, nColumns2, m1, m2)
 
-def solveAndOutput():
+def solveAndOutput(nRows1, nColumns1, nColumns2, m1, m2):
     print("Result:")
-    result = np.matmul(m1, m2)
-    for i in range(0, len(result)):
-        print(result[i])
+    finalM = [[0 for i in range(nColumns2)] for i in range(nRows1)]
+    for i in range(0, nRows1):
+        for j in range(0, nColumns2):
+            for k in range(0,nColumns1):
+                finalM[i][j] += m1[i][k] * m2[k][j]
+    for i in range(0, len(finalM)):
+        print(finalM[i])
+
 
 # Main program
 
